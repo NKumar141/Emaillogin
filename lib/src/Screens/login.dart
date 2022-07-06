0b086a0db51b44feb050bcdd5bb6b75e8a1adf6c
FlutLab.io <>
2022-07-06 03:41:12 +0000

    firebase adding

---
+++ lib/src/Screens/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Loginscreen extends StatefulWidget {
+  _LoginscreenState createState() => _LoginscreenState();
+}
+
+class _LoginscreenState extends State<Loginscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../src/Screens/login.dart';
+
+class app extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'login app',
+      theme: ThemeData(
+        accentColor: Colors.orange,
+        primarySwatch: Colors.blue,
+      ),
+      home: Loginscreen(),
+    );
+  }
+}
