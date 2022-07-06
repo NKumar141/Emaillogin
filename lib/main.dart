2915ffdf7ba47af8855bd9a41e720d0acd388ad4
FlutLab.io <>
2022-07-06 03:57:32 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,17 @@ import 'package:flutter/material.dart';
 import '../src/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_options/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      name: 'SecondaryApp',
+      options: const FirebaseOptions(
+        apiKey: '...',
+        appId: '...',
+        messagingSenderId: '...',
+        projectId: '...',
+      ));
   runApp(app());
 }
