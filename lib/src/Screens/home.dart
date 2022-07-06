baffea87df4e3c1145f1c2ed02d6b7868d29fc65
FlutLab.io <>
2022-07-06 14:41:38 +0000

    working signup

--- lib/src/Screens/home.dart
+++ lib/src/Screens/home.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Homescreen extends StatelessWidget {
   const Homescreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+          child: FlatButton(
+              child: Text('LogOut'),
+              onPressed: () {
+                auth.signOut();
+              })),
+    );
   }
 }
